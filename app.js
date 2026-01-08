9ffe249ef0de93308759602f82105a8c581c2006
rohit.kadam2 <rohit.kadam2@cipla.com>
2026-01-08 22:14:57 +0530

    added initial commit

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("test")
+const express = require("express");
+
+const app = express();
+const portNumber = 3000;
+
+app.get("/users/:id/:name", (req, res) => {
+    console.log(req.query)
+    console.log(req.params)
+    res.send("users get")
+})
+
+// app.post("/hello",(req, res)=>{
+//     res.send("hello world post")
+// })
+
+app.get("/",(req, res)=>{
+    res.send("///// get")
+})
+
+
+
+app.listen(portNumber,()=>{
+    console.log(`server is running on port ${portNumber}`)
+});
